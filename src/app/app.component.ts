f7715a73c11a2ef00ab4ef1139df82e6474dd7e8
moh <eng@mohamadarbash.com>
2019-10-23 22:36:32 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { TranslateAppService } from './core.mod/translate/services/translate-app
  */
 export class AppComponent {
   constructor(private translateAppService: TranslateAppService) {
-    console.log('fdffd');
+    console.log('fdffhhkjhkd');
     this.translateAppService.prepareLang();
   }
 }
