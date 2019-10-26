b560c6312cb3408cb0090dc2166a0009d40f13ff
moh <eng@mohamadarbash.com>
2019-10-26 18:12:04 +0200

    fix karma

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [ci.isCI ? 'ChromeJenkins' : 'Chrome'],
+    browsers: [!ci.isCI ? 'ChromeJenkins' : 'Chrome'],
     customLaunchers: {
       ChromeJenkins: {
         base: 'ChromeHeadless',
