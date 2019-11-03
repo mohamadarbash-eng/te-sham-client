9ff7d6d5cdd8056a8f9df265eb34be25c2fd5492
moh <eng@mohamadarbash.com>
2019-11-04 00:09:47 +0100

    test jenk.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,10 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 const ci = require('ci-info');
 
 module.exports = function (config) {
+  process.env.CHROME_BIN = require('puppeteer').executablePath();
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
