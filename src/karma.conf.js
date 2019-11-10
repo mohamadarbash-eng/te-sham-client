73b5b9f4eb543c2e834f535f010ef982151a28de
Arbash Mohamad <eng@mohamadarbash.com>
2019-11-10 22:29:55 +0100

    fix eh

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -2,7 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 const ci = require('ci-info');
-process.env.CHROME_BIN = '/usr/bin/google-chrome';
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
