a8806e45fc83971578e0f31234767fa1a3dfaca6
Arbash Mohamad <eng@mohamadarbash.com>
2019-11-11 00:16:22 +0100

    fix chrom

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const ci = require('ci-info');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+process.env.CHROMIUM_BIN = require('puppeteer').executablePath();
+console.log(process.env.CHROME_BIN);
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,10 +29,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [!ci.isCI ? 'ChromeJenkins' : 'Chrome'],
     hostname: 'localhost',
     listenAddress: 'localhost',
-    singleRun: true,
+    customLaunchers: {
+      ChromeJenkins: {
+        base: 'ChromiumHeadless',
+      },
+    },
+    singleRun: ci.isCI,
     concurrency: Infinity
   });
 };
